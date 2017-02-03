1c29120d1837f893b058d613c4f3bc9c4155f3ca
benjamin <ben@lafeldt.com>
2017-02-02 19:09:40 -0500

    Modified OK bot

--- OK2.py
+++ OK2.py
@@ -1,11 +1,12 @@
 import hlt
-from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square, opposite_cardinal
+from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
 import random
-
+import logging
 
 
 myID, game_map = hlt.get_init()
-hlt.send_init("RetreatBot")
+hlt.send_init("OverkillBot2")
+logging.basicConfig(filename='pydebug.log',level=logging.DEBUG)
 
 
 def find_nearest_enemy_direction(square):
@@ -14,13 +15,13 @@ def find_nearest_enemy_direction(square):
     for d in (NORTH, EAST, SOUTH, WEST):
         distance = 0
         current = square
-        while current.owner == (myID or 0) and distance < max_distance:
+        while current.owner == myID and distance < max_distance:
             distance += 1
             current = game_map.get_target(current, d)
         if distance < max_distance:
             direction = d
             max_distance = distance
-    return direction, max_distance
+    return direction
 
 def heuristic(square):
     if square.owner == 0 and square.strength > 0:
@@ -36,24 +37,26 @@ def get_move(square):
                                 default = (None, None),
                                 key = lambda t: heuristic(t[0]))
     if target is not None and target.strength < square.strength:
-        return Move(square, direction)
+        if game_map.get_target(square, direction) not in planned_moves:
+            planned_moves.append(game_map.get_target(square, direction))
+            return Move(square, direction)
     elif square.strength < square.production * 5:
         return Move(square, STILL)
 
     border = any(neighbor.owner != myID for neighbor in game_map.neighbors(square))
     if not border:
-        return Move(square, find_nearest_enemy_direction(square))
-    else: 
-        #wait until we are strong enough to attack
-		#if the nearest enemy is within 3, retreat. otherwise stay still
-        info = find_nearest_enemy_direction(square)
-        if info[1] > 2:
-            return Move(square, STILL)
-        else:
-            return Move(square, opposite_cardinal(info[0]))
+        if game_map.get_target(square, find_nearest_enemy_direction(square)) not in planned_moves:
+            planned_moves.append(game_map.get_target(square, find_nearest_enemy_direction(square)))
+            return Move(square, find_nearest_enemy_direction(square))
 
+    return Move(square, STILL)
+
+    
 while True:
+    planned_moves = []
     game_map.get_frame()
     moves = [get_move(square) for square in game_map if square.owner == myID]
+    for a in planned_moves:
+        logging.info(a)
     hlt.send_frame(moves)
 
