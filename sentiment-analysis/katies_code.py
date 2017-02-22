bcc9934fcab58de0201c6a62ace7542199972461
Katie Mulder <muldkate@users.noreply.github.com>
2017-02-21 21:57:26 -0500

    Update katies_code.py

--- sentiment-analysis/katies_code.py
+++ sentiment-analysis/katies_code.py
@@ -1,12 +1,15 @@
 
-# Team Asimov's Laws: Katie Mulder, Ben LaFeldt, Mattie Phillips
-# 
-# 
-# With an 80/20 split, the following code resulted in a 90.03% accuracy
-# on the data provided. The GridSearchCV selected the following paramters:
-#
-# The following lines were modified, with comments preceeding them.
-# Lines Modified: 35-74, 81-82, 90, 100, 104, 108-114
+# coding: utf-8
+
+# In[ ]:
+
+"""
+    Train a logistic regresion model for document classification.
+
+    Search this file for the keyword "Hint" for possible areas of
+    improvement.  There are of course others.
+"""
+print("Execution Started.")
 
 import pandas as pd
 import pickle
@@ -20,27 +23,32 @@ import sys
 import re
 import string
 from sklearn.linear_model import SGDClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import GridSearchCV
+
+# Hint: These are not actually used in the current 
+# pipeline, but would be used in an alternative 
+# tokenizer such as PorterStemming.
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 stop = stopwords.words('english')
- 
-print("Execution Started.")
 
-# Below are several tokenization strategies tried.
-# The default tokenization strategy yielded the highest accuracy.
+"""
+    This is a very basic tokenization strategy.  
+    
+    Hint: Perhaps implement others such as PorterStemming
+    Hint: Is this even used?  Where would you place it?
+"""
+
 porter = PorterStemmer()
-		
-# Remove only HTML tags from the text.
+
 def tokenizer(text):
-    text2 = re.sub("<.*>", " ", text)
+    #text2 = re.sub("<.*>", "", text)
+    return text.split()
+		
+def tokenizer2(text):
+    text2 = re.sub("<.*>", "", text)
     return text2.split()
 
-# Remove most punctuation marks and HTML tags from the text.
 def tokenizer_porter(text):
     text2 = re.sub("<.*>", "", text)
     text3 = re.sub('[/?/."!:<>(-)@#$%~&/*+,]*', "", text2)
@@ -52,17 +60,6 @@ def tokenizer_porter(text):
                 ary.append( porter.stem(word))
     return ary
 
-# Remove HTML tags and implment the tokenization using porter stemming.
-def tokenizer_porter_no_html(text):
-    text2 = re.sub("<.*>", "", text)
-    ary = []
-    for word in text4.split():
-        if len(word) > 2:
-            if word != "OED":
-                ary.append( porter.stem(word))
-    return ary
-
-# Remove most punctuation and implement tokenization using porter stemming.
 def tokenizer_porter_no_punct_html(text):
     text2 = re.sub("<.*>", "", text)
     text3 = re.sub('[/?/."!:<>(-)@#$%~&/*+,]*', "", text2)
@@ -78,41 +75,79 @@ def tokenizer_porter_no_punct_html(text):
 # Read in the dataset and store in a pandas dataframe
 df = pd.read_csv('./training_movie_data.csv')
 
-# Randomize the data
+
+#Randomize the data
 np.random.seed()
 df = df.reindex(np.random.permutation(df.index))
 
 # Split your data into training and test sets.
 # Allows you to train the model, and then perform
 # validation to get a sense of performance.
-
-# An 80/20  split of the data gave the most accurate split of data.
-# Other splits tried: 75/25, 70/30, and 90/10. 
+# 
+# Hint: This might be an area to change the size
+# of your training and test sets for improved 
+# predictive performance.
 training_size = 45000*.8
 X_train = df.loc[:training_size, 'review'].values
 y_train = df.loc[:training_size, 'sentiment'].values
 X_test = df.loc[training_size:, 'review'].values
 y_test = df.loc[training_size:, 'sentiment'].values
 
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import GridSearchCV
+
 # Perform feature extraction on the text.
-print("Creating Tokenizer.")
-# The parameters used below were parameters consistenly selected by the GridSearchCV, removed from the parameter grid
-# to limit the computation time required.
-tfidf = TfidfVectorizer(strip_accents='unicode', stop_words=None, tokenizer=None, lowercase=False, preprocessor=None)
+# Hint: Perhaps there are different preprocessors to test? 
+print("Starting Feature Extraction.")
+tfidf = TfidfVectorizer(strip_accents=ascii,
+						stop_words=None,
+						tokenizer=None,
+                        lowercase=False,
+                        preprocessor=None)
 
-# Another classifier that we tried was the SGDClassifier, but the LogisticRegression yielded the most accurate results.
+# Hint: There are methods to perform parameter sweeps to find the
+# best combination of parameters.  Look towards GridSearchCV in 
+# sklearn or other model selection strategies.
+
+# Create a pipeline to vectorize the data and then perform regression.
+# Hint: Are there other options to add to this process?
+# Look to documentation on Regression or similar methods for hints.
+# Possibly investigate alternative classifiers for text/sentiment.
 print("Starting pipeline and training")
-lr_tfidf = Pipeline([('vect', tfidf), ('clf', LogisticRegression(0))])
+lr_tfidf = Pipeline([('vect', tfidf),
+                     ('clf', LogisticRegression())
+					 #('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)),
+])
 
-# The below parameters yielded the most accuracy. 
-# Other Parameters explored: vect__min_df, vect__tokenizer, vect__stop_words, vect_lowercase, min_df, among others. 
 param_grid = [{
-		'vect__max_df': [.85, .9],
-              	'clf__penalty': ['l2'],
-		'clf__C': [5.0, 5.5, 6.0, 6.5]
-	     }]
-gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid, scoring='accuracy', cv=10, verbose=1, n_jobs=-1)
+				'vect__stop_words': [None],
+				'vect__strip_accents': [None, ascii],
+				'vect__tokenizer': [None],
+				'vect__lowercase': [False],
+				'vect__preprocessor': [None],
+         		#'vect__tokenizer': [None, tokenizer, tokenizer2],
+				#'vect__tokenizer': [None, tokenizer, tokenizer_porter_no_punct_html],
+              	#'clf__penalty': ['l1', 'l2', '13'],
+              	'clf__penalty': ['l1', 'l2'],
+				#'clf__C': [1.0, 10.0, 100.0]
+				#'clf__C': [10.0]
+              	'clf__C': [1.0, 5.0, 10.0, 15.0, 100.0]
+				#'clf_loss':['hinge'], 
+				#'clf_penalty':['l2'], 
+				#'clf__alpha':[.001, .0001], 
+				#'clf__n_iter':[1,5,10] 
+			}]
+
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
+                           scoring='accuracy',
+                           cv=5,
+                           verbose=10,
+                           n_jobs=-1)
+
 gs_lr_tfidf.fit(X_train, y_train)
+
 
 # Print the Test Accuracy
 print('CV Accuracy: %.8f' % gs_lr_tfidf.best_score_)
@@ -125,8 +160,12 @@ print (gs_lr_tfidf.best_params_)
 # Save the classifier for use later.
 pickle.dump(gs_lr_tfidf, open("saved_model.sav", 'wb'))
 
-print("Execution finished.")
+print("##########################################\n\n\nExecution finished.")
 
+      
+
+
+# In[ ]:
 
 
 
